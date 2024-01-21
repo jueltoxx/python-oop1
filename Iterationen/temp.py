c6967665092ca4ed2e4cd146548f79ee2f6c1099
joel buehler <joel.buehler@hftm.ch>
2024-01-21 13:48:45 +0100

    Verglecih klappt nur False noich nicht richtig

--- Iterationen/temp.py
+++ Iterationen/temp.py
@@ -1,10 +1,28 @@
-word1 = "abxz"
-word2 = "abx"
-n = 1
+word1 = input("Geben Sie Sting 1 ein: ")
+word2 = input("Geben Sie Sting 2 ein: ")
+match = True
 
-for letter in word1:
-    #print(letter)
-    letterw1 = letter
+#zählen ob glecih lang
+lenght1 = len(word1)
+lenght2 = len(word2)
 
-    if letterw1 == word2[n]:
-        print("Zeichen",n,"Matched")
+n = 0
+
+if lenght1 != lenght2:
+    print("FALSE: Strings matchen nicht. Länge passt nicht")
+else:
+    
+
+    for letter in word1:
+        #print(letter)
+        letterw1 = letter
+
+        if letterw1 == word2[n]:
+            #print("Zeichen",n,"Matched")
+            n = n + 1
+            if lenght2 == n:
+                print("TRUE: Die Strings sind gleich")
+                continue
+        else:
+            print("FALSE")
+            match = False
