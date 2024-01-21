11d68d495b519ca7932fd9cefa23bf24c9b2510d
joel buehler <joel.buehler@hftm.ch>
2024-01-21 14:02:59 +0100

    File umstrukturierung, keine Codeändeurng

--- Iterationen/Aufgabe5-compareString.py
+++ Iterationen/Aufgabe5-compareString.py
@@ -1,24 +1,28 @@
-string1 = input("Gebe String 1 ein: ")
-string2 = input("Gebe String 2 ein: ")
-
+word1 = input("Geben Sie Sting 1 ein: ")
+word2 = input("Geben Sie Sting 2 ein: ")
 match = True
-n = 0 
 
-while match == True:
-    for letter in string1:
-        letterstring1 = letter
+#zählen ob glecih lang
+lenght1 = len(word1)
+lenght2 = len(word2)
 
-        for letter in string2:
-            letterstring2 = letter
+n = 0
+
+if lenght1 != lenght2:
+    print("FALSE: Strings matchen nicht. Länge passt nicht")
+else:
+    for letter in word1:
+        #print(letter)
+        letterw1 = letter
         
-            if letterstring1 != letterstring2:
-                match = False
-                print("Strings nicht gleich!")
-            elif letterstring1 == letterstring2:
-
-                print("Match")
-                n += 1
-                
-        
-
-    
+        if letterw1 == word2[n]:
+            #print("Zeichen",n,"Matched")
+            n = n + 1
+            if lenght2 == n:
+                print("TRUE: Die Strings sind gleich")
+                continue
+        else:
+            print("FALSE")
+            match = False
+            quit()
+            
