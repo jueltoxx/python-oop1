f5aca19a7d9c940d50fe86eb7585a43391e0b9c1
joel buehler <joel.buehler@hftm.ch>
2024-01-21 13:16:30 +0100

    Status working on things..

--- Iterationen/temp.py
+++ Iterationen/temp.py
@@ -1,12 +1,11 @@
 word1 = "abxz"
 word2 = "abx"
-n = 0
-
+n = 1
 
 for letter in word1:
     #print(letter)
     letterw1 = letter
 
-    if letter == word2[n]:
+    if letterw1 == word2[n]:
         print("Zeichen",n,"Matched")
         n += 1
